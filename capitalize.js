aa53cf3d575da8c8c0b2f6e84113a1fe1f22e998
Panther-12 <nimrodwalwe@gmail.com>
2024-02-11 13:56:51 +0300

    capitalize.js

---
+++ capitalize.js
@@ -0,0 +1,15 @@
+/* Question 4: Capitalize Words
+    Write a program that accepts a string as input, capitalizes the first letter of each word in the
+    string, and then returns the result string. 
+*/
+
+const sampleString = "Hello, My name is Nimrod. A meta certified backend engineer"
+
+function capitalizeWords(str) {
+    let capitalizedString = str.replace(/\b\w/g, (char) => char.toUpperCase());
+    console.log(capitalizedString)
+    return capitalizedString
+}
+
+capitalizeWords(sampleString)
+
